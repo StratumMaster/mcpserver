e878f7910915384b52d2e28e92d5fca69d5c2063
StratumMaster <120847353+StratumMaster@users.noreply.github.com>
2025-07-13 11:45:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,3 @@ starlette_app = Starlette(
 
 # 5. Export starlette_app as app for uvicorn
 app = starlette_app
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
